ca3764b87dae14236cff4c7cb8a79e70b354c453
Ayushi-g42 <Ayushipatidar303@gmail.com>
2023-11-20 21:37:42 +0530

    Update style.js

--- src/components/rockets/style.js
+++ src/components/rockets/style.js
@@ -4,11 +4,20 @@ export const Container = styled.div`
   width: 100%;
   display: flex;
   height: 90vh;
+    max-width: 1280px;
+  margin: auto;
+
+.bg-image {
+    background-image: url("https://live.staticflickr.com/7706/26751237322_5a52540ea3_h.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    width: 84%;
+  }
   /* flex-wrap: wrap; */
 
   .main-section {
     width: 75%;
-    background-image: url("https://live.staticflickr.com/7706/26751237322_5a52540ea3_h.jpg");
     border-top-right-radius: 20px;
     border-bottom-right-radius: 20px;
     padding: 0px 90px 50px;
@@ -26,6 +35,7 @@ export const Container = styled.div`
       padding: 1rem;
       display: flex;
       flex-wrap: wrap;
+        justify-content: center;
     }
   }
 `;
